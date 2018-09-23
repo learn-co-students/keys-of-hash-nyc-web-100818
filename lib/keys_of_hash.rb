08a9039178b0fb27c7866b307a0a13c9ed640a6d
github username <github email address>
2018-09-23 11:50:34 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+require 'pry'
+
+# OBJECT ORIENTED PROGRAMMING W CLASSES 
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    self.map do |key, value|
+      if arguments.include?(value)
+        key
+      end
+    end.compact
   end
 end
+
+
+# self keyword
+# select iterator instead of each, and u won't need the compact
+
+# Reference:
+
+# key =  "sugar glider"
+# value = "sugar glider"
+# arguments = ["Panama"]
