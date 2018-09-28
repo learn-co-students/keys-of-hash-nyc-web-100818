421e085186778c54947fd1da6ad95c028d81c154
Cara Morelli <caramorelli7@gmail.com>
2018-09-28 19:18:55 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    new_obj = {}
+    self.each do |key, val|
+      if new_obj.has_key?(val)
+        new_obj[val] << key
+      else 
+        new_obj[val] = [key]
+      end 
+    end 
+    case arguments.length 
+    when 1 
+      new_obj[arguments[0]]
+    when 2 
+      arr = new_obj[arguments[0]]
+      new_obj[arguments[0]] += new_obj[arguments[1]]
+    end 
   end
 end
